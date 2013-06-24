6b3fdc093cb10c474ce9b92d153c5105cd27d1b5
Janar Todesk <janar@gsmvalve.ee>
2013-06-24 10:40:19 +0300

    Initial of line graphs simplification

--- js/types/lines.js
+++ js/types/lines.js
@@ -8,12 +8,56 @@ Flotr.addType('lines', {
     fillColor: null,       // => fill color
     fillOpacity: 0.4,      // => opacity of the fill color, set to 1 for a solid fill, 0 hides the fill
     steps: false,          // => draw steps
-    stacked: false         // => setting to true will show stacked lines, false will show normal lines
+    stacked: false,        // => setting to true will show stacked lines, false will show normal lines
+    maxPoints: 300         // => maximum number of points to render. 
   },
 
   stack : {
     values : []
   },
+
+  // Cache
+  previousScale: null,
+  simplifiedData: null,
+
+
+  /**
+   *
+   */
+  _simplify: function(data, options){
+
+    var
+      xScale = options.xScale,
+      yScale = options.yScale,
+      simplificationFactor = options.simplificationFactor,
+      _data = [];
+
+    var i = 0, length = data.length, segments = [], segment = [];
+
+    while(true){
+      // I see fail in my future
+      if(i >= length) break;
+
+      // If point's value is null, simplify the segment and push it to segments stack
+      if(data[i][1] === null){
+        if(segment.length >= 2) _data = _data.concat(simplify(segment, simplificationFactor), null);
+
+        segment = [];
+        ++i;
+        continue;
+      }
+
+      segment.push([xScale(data[i][0]), yScale(data[i][1]), data[i]]);
+
+      ++i;
+    }
+
+    // If there's something left in the segment object, stack it
+    if(segment.length >= 2) _data = _data.concat(simplify(segment, simplificationFactor), null);
+
+    return _data;
+  },
+
 
   /**
    * Draws lines series in the canvas element.
@@ -52,9 +96,9 @@ Flotr.addType('lines', {
   },
 
   plot : function (options, shadowOffset, incStack) {
-
     var
       context   = options.context,
+      series    = options.series,
       width     = options.width, 
       height    = options.height,
       xScale    = options.xScale,
@@ -66,80 +110,11 @@ Flotr.addType('lines', {
       prevy     = null,
       zero      = yScale(0),
       start     = null,
-      x1, x2, y1, y2, stack1, stack2, i;
+      x1, x2, y1, y2, stack1, stack2,
+      maxPoints = options.maxPoints,
+      simplificationFactor = width / maxPoints;
       
-    if (length < 1) return;
-
-    context.beginPath();
-
-    for (i = 0; i < length; ++i) {
-
-      // To allow empty values
-      if (data[i][1] === null || data[i+1][1] === null) {
-        if (options.fill) {
-          if (i > 0 && data[i][1] !== null) {
-            context.stroke();
-            fill();
-            start = null;
-            context.closePath();
-            context.beginPath();
-          }
-        }
-        continue;
-      }
-
-      // Zero is infinity for log scales
-      // TODO handle zero for logarithmic
-      // if (xa.options.scaling === 'logarithmic' && (data[i][0] <= 0 || data[i+1][0] <= 0)) continue;
-      // if (ya.options.scaling === 'logarithmic' && (data[i][1] <= 0 || data[i+1][1] <= 0)) continue;
-      
-      x1 = xScale(data[i][0]);
-      x2 = xScale(data[i+1][0]);
-
-      if (start === null) start = data[i];
-      
-      if (stack) {
-        stack1 = stack.values[data[i][0]] || 0;
-        stack2 = stack.values[data[i+1][0]] || stack.values[data[i][0]] || 0;
-        y1 = yScale(data[i][1] + stack1);
-        y2 = yScale(data[i+1][1] + stack2);
-        if (incStack) {
-          stack.values[data[i][0]] = data[i][1] + stack1;
-          if (i == length-1) {
-            stack.values[data[i+1][0]] = data[i+1][1] + stack2;
-          }
-        }
-      } else {
-        y1 = yScale(data[i][1]);
-        y2 = yScale(data[i+1][1]);
-      }
-
-      if (
-        (y1 > height && y2 > height) ||
-        (y1 < 0 && y2 < 0) ||
-        (x1 < 0 && x2 < 0) ||
-        (x1 > width && x2 > width)
-      ) continue;
-
-      if ((prevx != x1) || (prevy != y1 + shadowOffset)) {
-        context.moveTo(x1, y1 + shadowOffset);
-      }
-      
-      prevx = x2;
-      prevy = y2 + shadowOffset;
-      if (options.steps) {
-        context.lineTo(prevx + shadowOffset / 2, y1 + shadowOffset);
-        context.lineTo(prevx + shadowOffset / 2, prevy);
-      } else {
-        context.lineTo(prevx, prevy);
-      }
-    }
-    
-    if (!options.fill || options.fill && !options.fillBorder) context.stroke();
-
-    fill();
-
-    function fill () {
+    var fill = function(){
       // TODO stacked lines
       if(!shadowOffset && options.fill && start){
         x1 = xScale(start[0]);
@@ -148,11 +123,104 @@ Flotr.addType('lines', {
         context.lineTo(x1, zero);
         context.lineTo(x1, yScale(start[1]));
         context.fill();
-        if (options.fillBorder) {
-          context.stroke();
-        }
+        
+        if(options.fillBorder) context.stroke();
       }
     }
+
+    if (length < 1) return;
+
+    // If scale has changed (zoom has changed) resimplify the dataset
+    if(series._previousScale != series.xaxis.scale) series.simplifiedData = this._simplify(data, {xScale: xScale, yScale: yScale, simplificationFactor: simplificationFactor});
+    series._previousScale = series.xaxis.scale;
+
+    var
+      startIndex = null, endIndex = null,
+      i = series.simplifiedData.length - 1,
+      point,
+      startTime = options.xInverse(0),
+      endTime = options.xInverse(width);
+
+    // Determine start and end indexes to draw
+    while(i--){
+      if(series.simplifiedData[i] === null) continue;
+
+      point = series.simplifiedData[i][2];
+
+      if(endIndex === null && point[0] < endTime) endIndex = i;
+      if(startIndex === null && point[0] < startTime){
+        startIndex = i;
+        break;
+      }
+    }
+
+    // Normalize start and end indexes
+    startIndex = Math.max(0, startIndex - 1);
+    endIndex = Math.min(endIndex + 2, series.simplifiedData.length - 1);
+
+    // Now let's draw some!
+    context.beginPath();
+
+    for(var i = startIndex, point, nextPoint; i < endIndex; i++){
+      point = series.simplifiedData[i], nextPoint = series.simplifiedData[i+1];
+
+      // Allow for empty values
+      if(point == null || nextPoint == null){
+        if(options.fill){
+          if(i > 0){
+            context.stroke();
+            fill();
+            start = null;
+            context.closePath();
+            context.beginPath();
+          }
+        }
+
+        continue;
+      }
+
+      // Normalize points
+      point = point[2], nextPoint = nextPoint[2];
+
+      x1 = xScale(point[0]);
+      x2 = xScale(nextPoint[0]);
+
+      if(start === null) start = point;
+        
+      if(stack){
+        stack1 = stack.values[point[0]] || 0;
+        stack2 = stack.values[nextPoint[0]] || stack.values[point[0]] || 0;
+        y1 = yScale(point[1] + stack1);
+        y2 = yScale(nextPoint[1] + stack2);
+        if (incStack) {
+          stack.values[point[0]] = point[1] + stack1;
+          if (i == length-1) {
+            stack.values[nextPoint[0]] = nextPoint[1] + stack2;
+          }
+        }
+      }else{
+        y1 = yScale(point[1]);
+        y2 = yScale(nextPoint[1]);
+      }
+
+      if((prevx != x1) || (prevy != y1 + shadowOffset)) context.moveTo(x1, y1 + shadowOffset);
+      
+      prevx = x2, prevy = y2 + shadowOffset;
+
+      if(options.steps){
+        context.lineTo(prevx + shadowOffset / 2, y1 + shadowOffset);
+        context.lineTo(prevx + shadowOffset / 2, prevy);
+      }else{
+        //context.lineTo(prevx, prevy);
+        var xc = (x1 + x2) / 2;
+        var yc = (y1 + y2) / 2;
+        context.quadraticCurveTo(x1, y1, xc, yc);
+      }
+    }
+    
+    if (!options.fill || options.fill && !options.fillBorder) context.stroke();
+    
+    fill();
 
     context.closePath();
   },
